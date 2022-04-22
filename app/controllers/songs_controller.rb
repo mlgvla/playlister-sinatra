b51620c99d29f382a720ed133dcd1db8105cdee3
Monica Gerard <mlgvla@gmail.com>
2022-04-22 09:30:21 -0400

    changed has? to include? for flash

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
-    flash[:message] = "Successfully created song."
+    flash[:notice] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
 
@@ -40,7 +40,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
-    flash[:message] = "Successfully updated song."
+    flash[:notice] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
   end
 
